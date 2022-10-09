00e9d98140e420f71413066cf6ad885d3fb5d447
Dominik Bosnjak <dxb3501@g.rit.edu>
2022-10-09 14:47:21 +0200

    setting up stories component

--- practice-hacker-news/src/Stories.js
+++ practice-hacker-news/src/Stories.js
@@ -3,12 +3,38 @@ import React from "react";
 import { useGlobalContext } from "./context";
 
 const Stories = () => {
-  const { isLoading } = useGlobalContext();
+  const { isLoading, hits } = useGlobalContext();
 
   if (isLoading) {
     return <div className="loading">Loading</div>;
   }
-  return <h2>Stories component</h2>;
+  return (
+    <section className="stories">
+      {hits.map((story) => {
+        console.log(story);
+        const { objectID, title, num_comments, url, points, author } = story;
+        return (
+          <article className="story" key={objectID}>
+            <h4 className="title">{title}</h4>
+            <p className="info">
+              {points} points by <span>{author}</span> | {num_comments} commnets
+            </p>
+            <div className="">
+              <a
+                href={url}
+                className="read-link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Link to story
+              </a>
+              <button className="remove-btn"> remove</button>
+            </div>
+          </article>
+        );
+      })}
+    </section>
+  );
 };
 
 export default Stories;
